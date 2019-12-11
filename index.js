902ac26bc3dedfc96956ac8c24c7610a726167b9
MountainP <lspssapple@gmail.com>
2019-12-12 04:04:11 +0800

    login

--- index.js
+++ index.js
@@ -12,11 +12,11 @@ var user = "admin@gmail.com";
 var password = "1234";
 
 app.use(
-  cors({
-    origin: ["http://localhost:8080", "http://localhost:80"], //允許這個域的訪問
-    methods: ["GET", "POST"], //只允許GET和POST請求
-    alloweHeaders: ["Conten-Type", "Authorization"] //只允許帶這兩種請求頭的連接方式
-  })
+    cors({
+        origin: ["http://localhost:8080", "http://localhost:80"], //允許這個域的訪問
+        methods: ["GET", "POST"], //只允許GET和POST請求
+        alloweHeaders: ["Conten-Type", "Authorization"] //只允許帶這兩種請求頭的連接方式
+    })
 );
 
 app.use(history());
@@ -24,77 +24,116 @@ app.use(express.static(path.join(__dirname, "dist")));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.post("/api/users", function(req, res) {
-  var sql = "select * from users";
-  var params = [];
-  db.all(sql, params, (err, rows) => {
-    if (err) {
-      res.status(400).json({ error: err.message });
-      return;
-    }
-    res.json({
-      message: "success",
-      data: rows
+app.post("/api/users", function (req, res) {
+    var sql = "select * from users";
+    var params = [];
+    db.all(sql, params, (err, rows) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: "success",
+            data: rows
+        });
     });
-  });
 });
 
-app.post("/api/addUser", function(req, res) {
-  var errors = [];
-  if (!req.body.userPassword) {
-    errors.push("No password specified");
-  }
-  if (!req.body.userEmail) {
-    errors.push("No email specified");
-  }
-  if (errors.length) {
-    res.status(400).json({ error: errors.join(",") });
-    return;
-  }
-  var data = {
-    name: req.body.userName,
-    email: req.body.userEmail,
-    password: req.body.userPassword
-  };
-  var sql = "INSERT INTO users (name, email, password) VALUES (?,?,?)";
-  var params = [data.name, data.email, data.password];
-  db.run(sql, params, function(err, result) {
-    if (err) {
-      res.status(400).json({ error: err.message });
-      return;
+app.post("/api/addUser", function (req, res) {
+    var errors = [];
+    if (!req.body.userName) {
+        errors.push("No username specified");
     }
-    res.json({
-      message: "success",
-      data: data,
-      id: this.lastID
+    if (!req.body.userPassword) {
+        errors.push("No password specified");
+    }
+    if (!req.body.userEmail) {
+        errors.push("No email specified");
+    }
+    if (errors.length) {
+        res.status(400).json({ error: errors.join(",") });
+        return;
+    }
+    var data = {
+        name: req.body.userName,
+        email: req.body.userEmail,
+        password: req.body.userPassword
+    };
+    var sql = "INSERT INTO users (name, email, password) VALUES (?,?,?)";
+    var params = [data.name, data.email, data.password];
+    db.run(sql, params, function (err, result) {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: "success",
+            data: data,
+            id: this.lastID
+        });
     });
-  });
 });
 
 app.post("/api/loginCheck", (req, res, next) => {
-  let sql = `SELECT * FROM users WHERE name = "${req.body.userName}"`;
+    let sql = `SELECT * FROM users WHERE name = "${req.body.userName}"`;
 
-  db.all(sql, (err, rows) => {
-    if (err) {
-      res.status(500).send("Bad Database Request!");
-      return;
-    }
-    if (rows != "") {
-      rows.forEach(row => {
-        if (row.password == req.body.userPassword) {
-          res.status(200).send(rows);
-          return;
-        } else {
-          res.status(400).send("Wrong Password!");
-          return;
+    db.all(sql, (err, rows) => {
+        if (err) {
+            res.status(500).send("Bad Database Request!");
+            return;
         }
-      });
-    } else {
-      db.close();
-      res.status(400).send("Wrong UserName");
-      return;
-    }
-  });
+        if (rows != "") {
+            rows.forEach(row => {
+                if (row.password == req.body.userPassword) {
+                    res.status(200).send('Login!');
+                    return;
+                } else {
+                    res.status(400).send("Wrong Password!");
+                    return;
+                }
+            });
+        } else {
+            res.status(400).send("Wrong UserName");
+            return;
+        }
+    });
+});
+app.post("/api/getModify", (req, res, next) => {
+    let sql = `SELECT * FROM users WHERE name = "${req.body.userName}"`;
+
+    db.all(sql, (err, rows) => {
+        if (err) {
+            res.status(500).send("Bad Database Request!");
+            return;
+        }
+        rows.forEach(row => {
+            res.status(200).json(row);
+        });
+    });
+});
+
+app.post("/api/modify", (req, res, next) => {
+    let sql = `UPDATE users
+            SET password="${req.body.usesrPassword}",
+            email="${req.body.userEmail}"
+            WHERE name = "${req.body.userName}"`;
+
+
+    db.run(sql, (err) => {
+        if (err) {
+            res.status(500);
+            return console.error(err.message);
+
+        } else {
+            res.status(200);
+            return;
+        }
+        
+
+    });
+
+
+
 });
 
 /*
@@ -107,69 +146,69 @@ app.post("/api/loginCheck", function(req, res) {
     console.log("access denied");
   }
 });*/
-app.post("/api/db_add", function(req, res) {
-  fs.readFile("data.json", "utf8", function(err, data) {
-    var obj = JSON.parse(data);
-    obj.table.push(req.body);
-    var json = JSON.stringify(obj);
-    fs.writeFile("data.json", json, "utf8", function(err, data) {
-      console.log("add success");
+app.post("/api/db_add", function (req, res) {
+    fs.readFile("data.json", "utf8", function (err, data) {
+        var obj = JSON.parse(data);
+        obj.table.push(req.body);
+        var json = JSON.stringify(obj);
+        fs.writeFile("data.json", json, "utf8", function (err, data) {
+            console.log("add success");
+        });
+        res.send(obj.table);
+        //console.log(data);
     });
-    res.send(obj.table);
-    //console.log(data);
-  });
 });
 
-app.post("/api/db_select", function(req, res) {
-  fs.readFile("data.json", "utf8", function(err, data) {
-    var obj = JSON.parse(data);
-    res.send(obj.table);
-  });
-});
-app.post("/api/db_delete", function(req, res) {
-  fs.readFile("data.json", "utf8", function(err, data) {
-    var obj = JSON.parse(data);
-    var index_o = null;
-    obj.table.forEach(function(event, index) {
-      if (JSON.stringify(event) == JSON.stringify(req.body)) {
-        index_o = index;
-      }
+app.post("/api/db_select", function (req, res) {
+    fs.readFile("data.json", "utf8", function (err, data) {
+        var obj = JSON.parse(data);
+        res.send(obj.table);
     });
-    if (typeof index_o == "number") {
-      obj.table.splice(index_o, 1);
-      var json = JSON.stringify(obj);
-      fs.writeFile("data.json", json, "utf8", function(err, data) {
-        console.log("delete success");
-      });
-      res.send(obj.table);
-    } else {
-      res.sendStatus(500);
-    }
-  });
 });
-app.post("/api/db_update", function(req, res) {
-  fs.readFile("data.json", "utf8", function(err, data) {
-    var obj = JSON.parse(data);
-    var index_o = null;
-    obj.table.forEach(function(event, index) {
-      if (JSON.stringify(event) == JSON.stringify(req.body[0])) {
-        index_o = index;
-      }
+app.post("/api/db_delete", function (req, res) {
+    fs.readFile("data.json", "utf8", function (err, data) {
+        var obj = JSON.parse(data);
+        var index_o = null;
+        obj.table.forEach(function (event, index) {
+            if (JSON.stringify(event) == JSON.stringify(req.body)) {
+                index_o = index;
+            }
+        });
+        if (typeof index_o == "number") {
+            obj.table.splice(index_o, 1);
+            var json = JSON.stringify(obj);
+            fs.writeFile("data.json", json, "utf8", function (err, data) {
+                console.log("delete success");
+            });
+            res.send(obj.table);
+        } else {
+            res.sendStatus(500);
+        }
     });
-    if (typeof index_o == "number") {
-      obj.table.splice(index_o, 1, req.body[1]);
-      var json = JSON.stringify(obj);
-      fs.writeFile("data.json", json, "utf8", function(err, data) {
-        console.log("update success");
-      });
-      res.send(obj.table);
-    } else {
-      res.sendStatus(500);
-    }
-  });
+});
+app.post("/api/db_update", function (req, res) {
+    fs.readFile("data.json", "utf8", function (err, data) {
+        var obj = JSON.parse(data);
+        var index_o = null;
+        obj.table.forEach(function (event, index) {
+            if (JSON.stringify(event) == JSON.stringify(req.body[0])) {
+                index_o = index;
+            }
+        });
+        if (typeof index_o == "number") {
+            obj.table.splice(index_o, 1, req.body[1]);
+            var json = JSON.stringify(obj);
+            fs.writeFile("data.json", json, "utf8", function (err, data) {
+                console.log("update success");
+            });
+            res.send(obj.table);
+        } else {
+            res.sendStatus(500);
+        }
+    });
 
-  console.log(req.body);
+    console.log(req.body);
 });
 app.listen(80, () => {
-  console.log("app listening on port 80.");
+    console.log("app listening on port 80.");
 });
--- src/components/login.vue
+++ src/components/login.vue
@@ -5,19 +5,21 @@
         <v-flex shrink>
           <v-card min-width="350px" max-width="450px">
             <v-card-text>
-              <div>Record of the Day</div>
-              <p class="display-1 text--primary">Login</p>
-              <p>V-Calendar</p>
+              <div>紀錄生活中的一切...</div>
+              <v-divider/>
+              <p/>
+              <p class="display-2 text--primary">登入</p>
+              <p>行事曆</p>
             </v-card-text>
             <v-form v-model="valid">
               <v-container>
                 <v-row>
                   <v-col cols="12">
                     <v-text-field
-                      v-model="usermame"
+                      v-model="username"
                       :rules="usernameRules"
                       label="使用者名稱"
-                      :error="error"
+                      :error="errorName"
                       :error-messages="error_messages[0]"
                       required
                     ></v-text-field>
@@ -27,10 +29,10 @@
                   <v-col cols="12">
                     <v-text-field
                       v-model="password"
-                      :rules="usernameRules"
+                      :rules="passwordRules"
                       label="使用者密碼"
                       type="password"
-                      :error="error"
+                      :error="errorPass"
                       :error-messages="error_messages[1]"
                       required
                     ></v-text-field>
@@ -40,19 +42,49 @@
               <v-spacer></v-spacer>
               <v-card-actions>
                 <v-spacer></v-spacer>
-                <v-btn text icon color="primary" @click="login()">
+                <v-btn text icon color="primary" @click="joinDialog=true">
                   註冊
-                  <v-icon small>far fa-paper-plane</v-icon>
+                  <v-icon small>fas fa-user-plus</v-icon>
                 </v-btn>
                 <v-btn text icon color="primary" @click="login()">
                   登入
-                  <v-icon small>far fa-paper-plane</v-icon>
+                  <v-icon small>fas fa-sign-in-alt</v-icon>
                 </v-btn>
               </v-card-actions>
             </v-form>
           </v-card>
         </v-flex>
       </v-layout>
+
+      <v-row justify="center">
+        <v-dialog v-model="joinDialog" persistent max-width="600px">
+          <v-card>
+            <v-card-title>
+              <span class="headline">註冊帳號</span>
+            </v-card-title>
+            <v-card-text>
+              <v-container>
+                <v-row>
+                  <v-col cols="12">
+                    <v-text-field  v-model="joinName" label="帳號名稱" :rules="joinNameRules" required></v-text-field>
+                  </v-col>
+                  <v-col cols="12">
+                    <v-text-field  v-model="joinEmail" label="電子郵件" :rules="joinEmailRules"  required></v-text-field>
+                  </v-col>
+                  <v-col cols="12">
+                    <v-text-field   v-model="joinPass" label="密碼" type="password" :rules="joinPassRules"  required></v-text-field>
+                  </v-col>
+                </v-row>
+              </v-container>
+            </v-card-text>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn color="blue darken-1" text @click="joinDialog=false">取消</v-btn>
+              <v-btn color="blue darken-1" text @click="join()">註冊</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+      </v-row>
     </v-container>
   </transition>
 </template>
@@ -67,56 +99,73 @@ export default {
   name: "login",
   data() {
     return {
+      joinDialog: false,
       email: "",
       password: "",
       valid: false,
-      error: false,
+
+      joinName:"",
+      joinPass:"",
+      joinEmail:"",
+
       passwordRules: [
         v => !!v || "請輸入使用者密碼",
         v => v.length <= 20 || "Name must be less than 10 characters"
       ],
       username: "",
-      usernameRules: [v => !!v || "請輸入使用者帳號"]
+      usernameRules: [v => !!v || "請輸入使用者帳號"],
+      joinNameRules: [v => !!v || "請輸入使用者帳號"],
+      joinPassRules: [v => !!v || "請輸入使用者密碼"],
+      joinEmailRules: [v => !!v || "請輸入使用者信箱"],
+      error_messages: [],
+      errorPass: false,
+      errorName: false
     };
   },
   methods: {
     login() {
-      //write login authencation logic here!
-      var vm = this;
       axios
         .post("/loginCheck", {
-          userName: vm.username,
-          userPassword: vm.password
+          userName: this.username,
+          userPassword: this.password
         })
-        .then(function(response) {
+        .then(response => {
           localStorage.setItem("token", "ImLogin");
-          localStorage.setItem("username", vm.username);
-          vm.error = false;
-          vm.$router.push("/");
+          localStorage.setItem("username", this.username);
+          this.error = false;
+          this.$router.push("/");
         })
-        .catch(function(error) {
-          vm.error = true;
+        .catch(error => {
+          if (error.response.data == "Wrong UserName") {
+            this.error_messages[0] = "帳號不正確";
+            this.errorName = true;
+          }
+          if (error.response.data == "Wrong Password!") {
+            this.errorName = false;
+            this.error_messages[0] = "";
+            this.error_messages[1] = "密碼不正確";
+            this.errorPass = true;
+          }
+        });
+        
+    },
+      join() {
+      axios
+        .post("/addUser", {
+          userName: this.joinName,
+          userPassword: this.joinPass,
+          userEmail:this.joinEmail
+        })
+        .then(response => {
+          this.joinDialog=false;
+        })
+        .catch(response => {
           console.log(error);
         });
-
-      /*
-                if( this.email == 'm951406@gmail.com' && this.password == '12342234' ){
-                  localStorage.setItem('token', 'ImLogin');
-                  this.error = false;
-                  this.$router.push('/');
-                } else{
-                  this.error = true
-                }
-              */
+        
     }
   },
-  computed: {
-    error_messages() {
-      if (this.error == true) {
-        return ["帳號不正確", "密碼不正確"];
-      } else return [];
-    }
-  },
+  computed: {},
 
   mounted() {
     this.error = false;
